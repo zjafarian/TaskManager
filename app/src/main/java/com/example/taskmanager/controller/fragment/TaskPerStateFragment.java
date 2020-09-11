4396444b41fbbbaef0331b9e0dadb8b2d220a89f
zjafarian <zjafarian88@gmail.com>
2020-09-12 00:13:01 +0430

    Write Logic RecyclerView Tasks

--- app/src/main/java/com/example/taskmanager/controller/fragment/TaskPerStateFragment.java
+++ app/src/main/java/com/example/taskmanager/controller/fragment/TaskPerStateFragment.java
@@ -2,16 +2,33 @@ package com.example.taskmanager.controller.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.taskmanager.R;
+import com.example.taskmanager.model.Task;
+import com.example.taskmanager.model.User;
+import com.example.taskmanager.repository.IRepositoryTask;
+import com.example.taskmanager.repository.IRepositoryUser;
+import com.example.taskmanager.repository.TaskRepository;
+import com.example.taskmanager.repository.UserRepository;
+
+import java.util.List;
 
 
 public class TaskPerStateFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private IRepositoryTask mRepositoryTask;
+    private User mUser;
+    private List<Task> mTasks;
 
 
     public TaskPerStateFragment() {
@@ -30,6 +47,8 @@ public class TaskPerStateFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mRepositoryTask = TaskRepository.getInstance();
+
 
     }
 
@@ -37,6 +56,89 @@ public class TaskPerStateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_per_state, container, false);
+        View view = inflater.inflate(R.layout.fragment_task_per_state, container, false);
+        setViews(view);
+        initViews();
+        return view;
     }
+
+    private void setViews(View view) {
+        mRecyclerView = view.findViewById(R.id.recycler_view_task_list);
+    }
+
+    private void initViews() {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mTasks = mRepositoryTask.getTaskList();
+        TaskAdapter taskAdapter = new TaskAdapter(mTasks);
+        mRecyclerView.setAdapter(taskAdapter);
+    }
+
+    private class TaskHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTask;
+        private TextView mDateTask;
+        private Button mBtnTask;
+        private Task mTask;
+
+        public TaskHolder(@NonNull View itemView) {
+            super(itemView);
+            setViews(itemView);
+        }
+
+        private void setViews(@NonNull View itemView) {
+            mTitleTask = itemView.findViewById(R.id.text_title_task);
+            mDateTask = itemView.findViewById(R.id.text_date_task);
+            mBtnTask = itemView.findViewById(R.id.btn_first_title_task);
+        }
+
+        public void bindTask(Task task) {
+            mTask = task;
+            mTitleTask.setText(task.getTitleTask());
+            mDateTask.setText(task.getDateTask().toString());
+            String title = task.getTitleTask();
+            mBtnTask.setText(title.charAt(0));
+        }
+
+
+    }
+
+
+    private class TaskAdapter extends RecyclerView.Adapter<TaskHolder> {
+        private List<Task> mTasks;
+
+        public TaskAdapter(List<Task> taskList) {
+            mTasks = taskList;
+        }
+
+        public List<Task> getTasks() {
+            return mTasks;
+        }
+
+        public void setTasks(List<Task> tasks) {
+            mTasks = tasks;
+        }
+
+
+        @NonNull
+        @Override
+        public TaskHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.task_row_list, parent, false);
+            TaskHolder taskHolder =new TaskHolder(view);
+            return taskHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TaskHolder holder, int position) {
+            Task task = mTasks.get(position);
+            holder.bindTask(task);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mTasks.size();
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/taskmanager/repository/IRepositoryTask.java
+++ app/src/main/java/com/example/taskmanager/repository/IRepositoryTask.java
@@ -3,6 +3,7 @@ package com.example.taskmanager.repository;
 import com.example.taskmanager.model.Task;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.UUID;
 
 public interface IRepositoryTask extends Serializable {
@@ -11,5 +12,6 @@ public interface IRepositoryTask extends Serializable {
     void updateTask (Task task);
     void deleteTask (Task task);
     int getPosition (UUID taskId);
+    List<Task> getTaskList();
 
 }
