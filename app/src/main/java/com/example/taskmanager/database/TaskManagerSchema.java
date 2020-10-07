ec768db316135b2026a02fc8824b6156dbc3c4ca
zjafarian <zjafarian88@gmail.com>
2020-10-07 15:30:28 +0330

    create TaskDBHelper and create userTable and taskTable in database

--- app/src/main/java/com/example/taskmanager/database/TaskManagerSchema.java
+++ app/src/main/java/com/example/taskmanager/database/TaskManagerSchema.java
@@ -6,22 +6,27 @@ public class TaskManagerSchema {
 
     public static final class UserTable {
         public static final String NAME = "userTable";
-        public static final class userCols{
-            public static final String userId = "uuidUser";
+
+        public static final class userCols {
+            public static final String id = "idUser";
+            public static final String userUUID = "uuidUser";
             public static final String userName = "userName";
             public static final String password = "password";
 
         }
     }
 
-    public static final class TaskUser {
+    public static final class TaskTable {
         public static final String NAME = "taskTable";
-        public static final class taskCols{
-            public static final String taskId ="uuidTask";
-            public static final String userId = "uuidUser";
-            public static final String title= "titleTask";
+
+        public static final class taskCols {
+            public static final String idUser = "userId";
+            public static final String idTask = "taskId";
+            public static final String taskUUID = "uuidTask";
+            public static final String userUUID = "uuidUser";
+            public static final String title = "titleTask";
             public static final String description = "descriptionTask";
-            public static final String date ="dateTask";
+            public static final String date = "dateTask";
             public static final String state = "stateTask";
         }
     }
