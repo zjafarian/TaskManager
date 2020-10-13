ff7f2103bb60e7ef6ae827d1f9a443cf89dff9d1
zjafarian <zjafarian88@gmail.com>
2020-10-13 12:41:07 +0330

    Add Converters for convert UUID to String and Date to long
    add Converters to TaskManagerDatabase

---
+++ app/src/main/java/com/example/taskmanager/database/Converters.java
@@ -0,0 +1,32 @@
+package com.example.taskmanager.database;
+
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Converters {
+
+    @TypeConverter
+    public static Date timestampToDate(long timeStamp) {
+        return new Date(timeStamp);
+    }
+
+    @TypeConverter
+    public static Long dateToTimestamp(Date date) {
+        return date.getTime();
+    }
+
+    @TypeConverter
+    public static String uuidToString(UUID uuid){
+        return uuid.toString();
+    }
+
+    @TypeConverter
+    public static UUID stringToUUID (String uuid){
+        return UUID.fromString(uuid);
+    }
+
+
+
+}
--- app/src/main/java/com/example/taskmanager/database/TaskManagerDatabase.java
+++ app/src/main/java/com/example/taskmanager/database/TaskManagerDatabase.java
@@ -1,7 +1,14 @@
 package com.example.taskmanager.database;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
+import com.example.taskmanager.model.Task;
+import com.example.taskmanager.model.User;
+
+@Database(entities = {Task.class, User.class}, version = 1)
+@TypeConverters({Converters.class})
 public abstract class TaskManagerDatabase extends RoomDatabase {
 
     public abstract TaskDAO getTaskTable();
